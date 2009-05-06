d0648dd1d1f3d87166b73473fac6eae63fc8220b
Tomek Wojcik <vooydzig@o2.pl>
2009-05-06 21:55:03 +0200

    maly update

--- Source/ass8_portal/friends/urls.py
+++ Source/ass8_portal/friends/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('friends.views',
-    url(r'index/(?P<username>[a-zA-Z0-9_]+)/$','friends_manage'),
-    url(r'followers/(?P<username>[a-zA-Z0-9_]+)/$','friend_list',{'list_type':'followers'}),
-    url(r'following/(?P<username>[a-zA-Z0-9_]+)/$','friend_list',{'list_type':'following'}),
-    url(r'mutual/(?P<username>[a-zA-Z0-9_]+)/$','friend_list',{'list_type':'mutual'}),
-    url(r'add/(?P<username>[a-zA-Z0-9_]+)/$','add_friend'),
-    url(r'delete/(?P<username>[a-zA-Z0-9_]+)/$','del_friend'),
+    url(r'index/(?P<username>[\w-]+)/$','friends_manage'),
+    url(r'followers/(?P<username>[\w-]+)/$','friend_list',{'list_type':'followers'}),
+    url(r'following/(?P<username>[\w-]+)/$','friend_list',{'list_type':'following'}),
+    url(r'mutual/(?P<username>[\w-]+)/$','friend_list',{'list_type':'mutual'}),
+    url(r'add/(?P<username>[\w-]+)/$','add_friend'),
+    url(r'delete/(?P<username>[\w-]+)/$','del_friend'),
 )
